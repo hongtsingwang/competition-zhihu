62426d72a4c2db5c2a9a92167699b873545ac2d0
hongtsingwang <hongtsingwang@gmail.com>
2017-05-16 18:49:04 +0800

    test script file

--- first_script.py
+++ first_script.py
@@ -26,6 +26,10 @@ sc = SparkContext("yarn-client", "zhihu first predict")
 hadoop_base_dir = "/data/rec/wanghongqing/competition-zhihu"
 original_data_dir = os.path.join(hadoop_base_dir, "original_data")
 assert os.path.exists(original_data_dir)
-print "234"
+
+question_topic_train_set_file = os.path.join(original_data_dir, "question_topic_train_set.txt")
+question_topic_train = sc.textFile(question_topic_train_set_file).cache()
+
+print "line number of the input file is %d" % (question_topic_train.count())
 
 sc.stop()
